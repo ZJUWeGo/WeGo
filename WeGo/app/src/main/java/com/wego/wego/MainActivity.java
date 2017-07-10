d619fc81946b2869384cd9d94c6ba0b32bdb626d
hongyeliang <xialuokecaocao@outlook.com>
2017-07-10 13:01:13 +0800

    list items has finished

--- WeGo/app/src/main/java/com/wego/wego/MainActivity.java
+++ WeGo/app/src/main/java/com/wego/wego/MainActivity.java
@@ -357,16 +357,11 @@ public class MainActivity extends AppCompatActivity
 
                 String unicodeString = temp.get("item_name").toString();
 
-                try {
-                    // Convert from Unicode to UTF-8
-                    byte[] utf8 = unicodeString.getBytes("UTF-8");
-                    // Convert from UTF-8 to Unicode
-                    unicodeString = new String(utf8, "UTF-8");
-                    System.out.println(unicodeString);
-                    } catch (UnsupportedEncodingException e) {
-                }
+                String utf8String = unicodeToUtf8(unicodeString);
 
-                String tempString = unicodeString + "   "+ "数量："+  temp.get("item_num").toString();
+                System.out.println(utf8String);
+
+                String tempString = utf8String + "   "+ "数量："+  temp.get("item_num").toString();
 
                 data.add(tempString);
             }
@@ -392,4 +387,76 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+
+    /**
+     * unicode 转换成 utf-8
+     * @author fanhui
+     * 2007-3-15
+     * @param theString
+     * @return
+     */
+    public static String unicodeToUtf8(String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len);
+        for (int x = 0; x < len;) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+                if (aChar == 'u') {
+                    // Read the xxxx
+                    int value = 0;
+                    for (int i = 0; i < 4; i++) {
+                        aChar = theString.charAt(x++);
+                        switch (aChar) {
+                            case '0':
+                            case '1':
+                            case '2':
+                            case '3':
+                            case '4':
+                            case '5':
+                            case '6':
+                            case '7':
+                            case '8':
+                            case '9':
+                                value = (value << 4) + aChar - '0';
+                                break;
+                            case 'a':
+                            case 'b':
+                            case 'c':
+                            case 'd':
+                            case 'e':
+                            case 'f':
+                                value = (value << 4) + 10 + aChar - 'a';
+                                break;
+                            case 'A':
+                            case 'B':
+                            case 'C':
+                            case 'D':
+                            case 'E':
+                            case 'F':
+                                value = (value << 4) + 10 + aChar - 'A';
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                        "Malformed   \\uxxxx   encoding.");
+                        }
+                    }
+                    outBuffer.append((char) value);
+                } else {
+                    if (aChar == 't')
+                        aChar = '\t';
+                    else if (aChar == 'r')
+                        aChar = '\r';
+                    else if (aChar == 'n')
+                        aChar = '\n';
+                    else if (aChar == 'f')
+                        aChar = '\f';
+                    outBuffer.append(aChar);
+                }
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
+    }
 }
