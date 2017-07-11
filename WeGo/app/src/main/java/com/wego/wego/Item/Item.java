fba1746349d371d051e146c8a70667405ca1690d
朱八八 <z19964950@gmail.com>
2017-07-11 13:29:42 +0800

    WeGo-item?

---
+++ WeGo/app/src/main/java/com/wego/wego/Item/Item.java
@@ -0,0 +1,63 @@
+package com.wego.wego.Item;
+
+/**
+ * Created by a4950 on 2017/7/10.
+ */
+
+public class Item {
+    private String itemName;  //商品名
+    private String itemNumber;  //商品编号
+    private double itemPrice;  //商品价格
+    private int itemQuantity;  //商品数量
+
+    public Item(){
+        itemName = "无";
+        itemNumber = "0";
+        itemPrice = 0;
+        itemQuantity = 0;
+    }
+
+    public Item(String itemText){
+        String[]item = itemText.split("-");
+        itemName = item[0];
+        itemNumber = item[1];
+        itemPrice = Double.parseDouble(item[2]);
+        itemQuantity = 1;
+        //System.out.println(itemPrice+"!!!!");
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+    public String getItemNumber(){
+        return itemNumber;
+    }
+    public Double getItemPrice(){
+        return itemPrice;
+    }
+    public int getItemQuantity(){
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int num){
+        itemQuantity = num;
+    }
+
+    @Override
+    public boolean equals(Object x) {
+        if (this == x) return true;
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
+        Item s = (Item)x;
+        if (!getItemNumber().equals(s.getItemNumber())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        String itemString = getItemName() + "-" + getItemNumber() + "-" + getItemPrice() + "*" + getItemQuantity();
+        return itemString;
+    }
+}
---
+++ WeGo/app/src/main/java/com/wego/wego/receiveItem.java
@@ -0,0 +1,17 @@
+package com.wego.wego;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wego.wego.Base.BaseNfcActivity;
+
+public class receiveItem extends BaseNfcActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_item);
+
+
+    }
+}
