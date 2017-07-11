c86a187bcff52ed3601b338a444f20deff8fe02c
朱八八 <z19964950@gmail.com>
2017-07-11 14:06:44 +0800

    WeGo-通过nfc收到商品列表

--- WeGo/app/src/main/java/com/wego/wego/MainActivity.java
+++ WeGo/app/src/main/java/com/wego/wego/MainActivity.java
@@ -4,8 +4,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
 import android.provider.Settings;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -33,6 +39,8 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wego.wego.Base.BaseNfcActivity;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,6 +48,7 @@ import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -49,7 +58,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseNfcActivity
         implements NavigationView.OnNavigationItemSelectedListener, Runnable{
     private String email;
     private String password;
@@ -70,7 +79,7 @@ public class MainActivity extends AppCompatActivity
 
         //TextView AccountName = (TextView) LayoutInflater.from(AccountActivity.this).inflate(R.layout.nav_header_account, null).findViewById(R.id.Account_name);
 
-        TextView AccountName = (TextView)findViewById(R.id.textView4);
+        TextView AccountName = (TextView)findViewById(R.id.Account_name);
         Bundle bundle = intent.getExtras();
         email = bundle.getString("email");
         password = bundle.getString("password");
@@ -682,4 +691,85 @@ public class MainActivity extends AppCompatActivity
         }
         return outBuffer.toString();
     }
+
+
+    private String mTagText;
+
+    @Override
+    public void onNewIntent(Intent intent){
+        //获取Tag对象
+        mTagText = "";
+        Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        //获取Ndef的实例
+        Ndef ndef = Ndef.get(detectedTag);
+        readNfcTag(intent);
+        System.out.println("收到了！！！！！！"+mTagText);
+    }
+
+    private void readNfcTag(Intent intent){
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())){
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                    NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage msgs[] = null;
+            int contentSize = 0;
+            if(rawMsgs != null){
+                msgs = new NdefMessage[rawMsgs.length];
+                for(int i = 0; i < rawMsgs.length; i++){
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                    contentSize += msgs[i].toByteArray().length;
+                }
+            }
+            try {
+                if (msgs != null) {
+                    NdefRecord record = msgs[0].getRecords()[0];
+                    String textRecord = parseTextRecord(record);
+                    mTagText += textRecord;
+                }
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+
+
+    /**
+     * 解析NDEF文本数据，从第三个字节开始，后面的文本数据
+     * @param ndefRecord
+     * @return
+     */
+    public static String parseTextRecord(NdefRecord ndefRecord) {
+        /**
+         * 判断数据是否为NDEF格式
+         */
+        //判断TNF
+        if (ndefRecord.getTnf() != NdefRecord.TNF_WELL_KNOWN) {
+            return null;
+        }
+        //判断可变的长度的类型
+        if (!Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+            return null;
+        }
+        try {
+            //获得字节数组，然后进行分析
+            byte[] payload = ndefRecord.getPayload();
+            //下面开始NDEF文本数据第一个字节，状态字节
+            //判断文本是基于UTF-8还是UTF-16的，取第一个字节"位与"上16进制的80，16进制的80也就是最高位是1，
+            //其他位都是0，所以进行"位与"运算后就会保留最高位
+            String textEncoding = ((payload[0] & 0x80) == 0) ? "UTF-8" : "UTF-16";
+            //3f最高两位是0，第六位是1，所以进行"位与"运算后获得第六位
+            int languageCodeLength = payload[0] & 0x3f;
+            //下面开始NDEF文本数据第二个字节，语言编码
+            //获得语言编码
+            String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            //下面开始NDEF文本数据后面的字节，解析出文本
+            String textRecord = new String(payload, languageCodeLength + 1,
+                    payload.length - languageCodeLength - 1, textEncoding);
+            return textRecord;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
+
